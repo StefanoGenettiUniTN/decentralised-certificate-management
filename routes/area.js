d07f5308b68d9a27d1c0adc0a85bf6e56b24f970
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-06-24 20:34:43 +0200

    Added method to retrieve all the areas

---
+++ routes/area.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Areas = require('../models/area');
+
+router.get('/areas', async (req, res) => {
+    let areas = await Areas.find({});
+
+    let response = areas.map( (area) => {
+        return {
+            self: "/api/v1/areas/"+area.area_id,
+            name: area.name
+        };
+    });
+    res.status(200).json(response);
+});
