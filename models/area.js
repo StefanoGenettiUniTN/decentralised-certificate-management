0b6b53906bb5bb4713422326dce09c0991745be1
Eros <erosribaga@hotmail.it>
2023-06-29 16:10:45 +0200

    Revert "Fix profile page"

--- models/area.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = mongoose.Schema({
-    area_id: Number,
-    name: String
-},{collection: 'area'})
-
-module.exports = mongoose.model("Area", schema)
